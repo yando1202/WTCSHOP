7c6d4c8a7198feac23a298f2d80295d9b39190af
yando1202 <yando.han@gmail.com>
2014-12-05 23:49:16 +0700

    versiPertama

---
+++ src/com/kevinyando/wtcshop/MainActivity.java
@@ -0,0 +1,48 @@
+package com.kevinyando.wtcshop;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	public void openBreadtalk(View v){
+		Intent i = new Intent(getApplicationContext(),BreadtalkActivity.class);
+		startActivity(i);
+	}
+	public void openJco(View w){
+		Intent j = new Intent(getApplicationContext(),JcoActivity.class);
+		startActivity(j);
+	}
+	public void openSingaporeoice(View x){
+		Intent k = new Intent(getApplicationContext(),SingaporeoiceActivity.class);
+		startActivity(k);
+	}
+}
